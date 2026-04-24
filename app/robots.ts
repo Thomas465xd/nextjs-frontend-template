77955635cb04136be9171642ee1e52de9496a93b
Thomas Schrödinger <160943727+Thomas465xd@users.noreply.github.com>
2026-04-23 20:33:37 -0400

    * Defined custom utility classes in globals.css for consistent styling and SEO related files.     * settings.json:         - Disabled tailwind linter "suggestCannonicalClasses" warning.     * globals.css:         - Defined classes for custom scrollbar.         - Created various custom utility classes:             - flex-center             - flex-between             - flex-align             - flex-center-col             - invisible-button         - Base styles for light and dark theme classes     * manifest.ts:         - Defines the web app manifest for PWA capabilities, including icons and theme colors.     * robots.ts:         - Configures the robots.txt file to control web crawler access to the site.         - Disallows web crawlers from accessing /admin directories.         - Points to the sitemap for better indexing.     * globals.d.ts:         - added declare module for all *.css files to allow importing CSS modules without TypeScript errors.

---
+++ app/robots.ts
@@ -0,0 +1,17 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+    return {
+        rules: [
+            {
+                userAgent: "*",
+                allow: "/",
+                disallow: [
+                    "/admin",
+                    "/admin/*",
+                ],
+            },
+        ],
+        sitemap: `${process.env.NEXT_PUBLIC_FRONTEND_URL ?? "https://mywebsite.com"}/sitemap.xml`,
+    };
+}
